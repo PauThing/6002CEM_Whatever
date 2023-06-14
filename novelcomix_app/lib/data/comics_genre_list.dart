7bb8ddbfcafeb3e45dd1d4d6be9c2bc961e16dec
Teriyaki <103395356+PauThing@users.noreply.github.com>
2023-06-14 16:43:19 +0800

    Comic model and data

---
+++ novelcomix_app/lib/data/comics_genre_list.dart
@@ -0,0 +1,29 @@
+import 'package:novelcomix_app/models/comics_genre.dart';
+
+List<ComicsGenre> comicGenreList = [
+  ComicsGenre(
+    comicGenre: "Romance",
+    imageSource:
+        "https://imgg.mangaina.com/bb06b04ee9056743ffac77791af035a5.webp",
+  ),
+  ComicsGenre(
+    comicGenre: "Adventure",
+    imageSource:
+    "https://img.freepik.com/premium-photo/man-gym-with-light-his-back_780838-576.jpg",
+  ),
+  ComicsGenre(
+    comicGenre: "Fantasy",
+    imageSource:
+    "https://img.freepik.com/premium-photo/man-gym-with-light-his-back_780838-576.jpg",
+  ),
+  ComicsGenre(
+    comicGenre: "Action",
+    imageSource:
+    "https://img.freepik.com/premium-photo/man-gym-with-light-his-back_780838-576.jpg",
+  ),
+  ComicsGenre(
+    comicGenre: "Drama",
+    imageSource:
+    "https://img.freepik.com/premium-photo/man-gym-with-light-his-back_780838-576.jpg",
+  ),
+];
--- novelcomix_app/lib/models/comics_genre.dart
+++ novelcomix_app/lib/models/comics_genre.dart
@@ -1,27 +1,9 @@
 class ComicsGenre {
-  final String name;
-  final String category;
-  final List<String> steps;
-  final String sets;
-  final String reps;
-  final String duration;
-  final String imageUrl;
-  final List<String> equipment;
-  final List<String> targetMuscles;
-  final int difficulty;
-  bool isFavourite;
+  final String imageSource;
+  final String comicGenre;
 
   ComicsGenre({
-    required this.category,
-    required this.duration,
-    required this.imageUrl,
-    required this.name,
-    required this.reps,
-    required this.sets,
-    required this.steps,
-    required this.equipment,
-    required this.targetMuscles,
-    required this.difficulty,
-    this.isFavourite = false,
+    required this.imageSource,
+    required this.comicGenre,
   });
 }
