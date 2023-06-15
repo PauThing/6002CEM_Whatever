f4a7b66aebf4841745557f52a4756ab981440ca2
CrazyJoey4 <crazyjoeyooi@gmail.com>
2023-06-15 23:12:52 +0800

    Update Novel data

---
+++ novelcomix_app/lib/data/novel.dart
@@ -0,0 +1,223 @@
+import 'package:novelcomix_app/models/novel_model.dart';
+
+List<NovelModel> comicList = [
+  //Romance
+  NovelModel(
+    title: 'Lethal Romance',
+    genre: 'Romance',
+    chapters: 5,
+    description: 'A woman who is like a martial arts manhwa character "Yubi". A man who is like a shoujo manhwa character "Shinwoo". The two who are used to living in their own genres won’t easily fall for each other! Will they be able to achieve a happy ending according to the standard of manhwa?!',
+    author: 'Grrr',
+    publication: '2020',
+    status: 'Completed',
+    imageUrl:
+        'https://mangadex.org/covers/6e9f81c1-a093-4ee0-8894-13c32586406c/d980b34d-cd0a-4417-8b30-8df68b25dacf.jpg.512.jpg',
+  ),
+  NovelModel(
+    title: 'Rebirth After Divorce',
+    genre: 'Romance',
+    chapters: 23,
+    description: 'Lu Xiao would never expect that his lover for seven years whom he loved deeply will betrayed him with his ‘good’ cousin.\n\nAnd the thing that he couldn’t accept the most was the fact he caught them cheating on his wedding anniversary. At that time, both his mind and body were engulfed in extreme emotions. He quickly slam the door open and leave the place where he used to live for years.',
+    author: 'Kiss Thorns',
+    publication: '2014',
+    status: 'Completed',
+    imageUrl:
+        'https://cdn.novelupdates.com/images/2023/04/Rebirth-After-Divorce_1681748557.jpg',
+  ),
+  NovelModel(
+    title: 'A Blessed Daughter',
+    genre: 'Romance',
+    chapters: 16,
+    description: 'Wei Ruo, a highly skilled doctor, was time traveled as a female supporting role in a novel. She was left neglected in the countryside for ten years and returned back when she was thirteen years old.',
+    author: 'Er Feng Chong',
+    publication: '2022',
+    status: 'Ongoing',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2023/05/A-Blessed-Daughter.jpg',
+  ),
+  NovelModel(
+    title: 'A Cat with a Red Envelope Group',
+    genre: 'Romance',
+    chapters: 52,
+    description: 'Died young! The second day after Xu Mao Mao rejected his blind date, he turned into that guy’s cat!',
+    author: 'Si Twenty-One',
+    publication: '2018',
+    status: 'Completed',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2018/05/006dtLZFgy1fn4l1r4w1wj305k07swef.jpg',
+  ),
+  NovelModel(
+    title: 'A Guide to a Perfect Death',
+    genre: 'Romance',
+    chapters: 50,
+    description: '“You won’t regret it?”\n\n“Captain, do not regret it.”\n\nThe first SS-class knight, Christa Palmaril.\n\nA slayer with monstrous healing powers that cannot be killed by any torture.\n\nShe eventually died by cutting off her throat with a holy sword. No, she should have died.',
+    author: 'Cheong Su-Pil',
+    publication: '2021',
+    status: 'Completed',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2022/09/A-Guide-to-a-Perfect-Death.jpg',
+  ),
+
+  //Adventure
+  NovelModel(
+    title: 'All The Bigshots Love Me',
+    genre: 'Adventure',
+    chapters: 69,
+    description: 'She is his cure, so sweet, spoiled, and full of teasing.',
+    author: 'Blooming Without Results',
+    publication: '2018',
+    status: 'Completed',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2020/11/20200610221919.jpg',
+  ),
+  NovelModel(
+    title: 'Blood Princess, Kill Them All',
+    genre: 'Adventure',
+    chapters: 15,
+    description: 'Mary is a princess who lacks the ability to use magic.\n\nHer arranged political marriage was set in stone, and she hoped to finally serve a purpose as a member of the royal family. However, during the engagement party, her engagement was abruptly broken off, and she was falsely accused of a crime and sentenced to execution.',
+    author: 'kiki',
+    publication: '2021',
+    status: 'Completed',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2022/08/Blood-Princess-Kill-Them-All.jpg',
+  ),
+  NovelModel(
+    title: 'Breakthrough with the Forbidden Master',
+    genre: 'Adventure',
+    chapters: 5,
+    description: '“Compared to our time, the current generation just aren’t as reliable” they said, “Still, isn’t he the son of the Hero?” How annoying.',
+    author: 'Anikki Burazza',
+    publication: '2019',
+    status: 'Ongoing',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2020/11/81xk8lduvjl._ac_sl1500_.jpg',
+  ),
+
+
+  //Fantasy
+  NovelModel(
+    title: 'Children of the Rune – Winterer',
+    genre: 'Fantasy',
+    chapters: 5,
+    description: 'A thousand years have passed since the great ancient kingdom suddenly fell to ruin for reasons unknown and became the ruins known as the Mortal Land. It is said that four weapons had become the keys to the kingdom’s destruction. Meanwhile, a young boy is left all alone in the midst of political strife and conflict and is fleeing from his enemies while clutching the sword Winterer, his house’s treasure that was passed down to him, in his hands.',
+    author: 'Jeon Min Hee',
+    publication: '2001',
+    status: 'Completed',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2023/04/Children-of-the-Rune-Winterer.jpg',
+  ),
+  NovelModel(
+    title: 'Company Employee Who Sees Destiny',
+    genre: 'Fantasy',
+    chapters: 3,
+    description: 'A child born with a destiny to become a shaman and disturb the world. In order not to be consumed by greed and to refuse the calling of the spirits, he tries to become an ordinary company employee.\n\nChoi Young-hoon, who foresees destiny through physiognomy and astrology. Can he really become an ordinary company employee?',
+    author: 'Yeong-Wan',
+    publication: '2019',
+    status: 'Completed',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2023/06/Company-Employee-Who-Sees-Destiny.jpg',
+  ),
+  NovelModel(
+    title: 'Dad, Did You Study Today?',
+    genre: 'Fantasy',
+    chapters: 6,
+    description: 'Zhou Qing: Not to mention her crossing, even the father of the original owner was also a transmigrator, an ancient dandy!',
+    author: 'Apple Lady',
+    publication: '2020',
+    status: 'Ongoing',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2023/06/Dad-Did-You-Study-Today.jpg',
+  ),
+  NovelModel(
+    title: 'Genius Developer Becomes a Tycoon',
+    genre: 'Fantasy',
+    chapters: 2,
+    description: 'With my regression, I have become a genius intern. It feels like being a max level character wandering around a beginner’s zone. With this regression, I aspire to become a tycoon.',
+    author: 'Pu-Dal',
+    publication: '2021',
+    status: 'Ongoing',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2023/05/Genius-Developer-Becomes-a-Tycoon.jpeg',
+  ),
+
+  //Action
+  NovelModel(
+    title: 'A Girl Who Was Told “You Have No Talent” Turned Out To Be A Talented Monster',
+    genre: 'Action',
+    chapters: 163,
+    description: 'A certain girl aspiring to be an adventurer was invited to join the Hero’s Party by chance and became their apprentice. However, she was not actually treated as a real apprentice, and was subjected to physical violence and verbal abuse every day.',
+    author: 'Ratimu',
+    publication: '2020',
+    status: 'Ongoing',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2022/09/A-Girl-Who-Was-Told-You-Have-No-Talent-Turned-Out-To-Be-A-Talented-Monster_1662179912.jpg',
+  ),
+  NovelModel(
+    title: 'Absolute Sword Sense',
+    genre: 'Action',
+    chapters: 210,
+    description: 'After being given up on by his family because his dantian was destroyed, Soh Woonhwi was kidnapped by the Blood Cult and lived as a third-rate spy.\n\mOne day, he died while being used to find a legendary sword immortal’s secret records. However, Soh returned to the day he was kidnapped ten years ago and obtained a mysterious ability that allowed him to hear the voice of swords.',
+    author: 'Hanjung-Wol-Ya',
+    publication: '2019',
+    status: 'Completed',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2022/07/Absolute-Sword-Sense.jpeg',
+  ),
+  NovelModel(
+    title: 'Academy’s Second Seat',
+    genre: 'Action',
+    chapters: 48,
+    description: 'He became the villain in the possessed game, the rival of the main character, and the second seat.',
+    author: 'Yunmaemi',
+    publication: '2021',
+    status: 'Completed',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2022/09/Academys-Second-Seat.jpg',
+  ),
+  NovelModel(
+    title: 'Almighty',
+    genre: 'Action',
+    chapters: 548,
+    description: 'Yang Tian’s clan is slaughtered overnight without any prior omens or any reason he can comprehend. Strangely, their ally clan, Li Clan, doesn’t send them any help. His parents hide him in a dimension during the attack to save his life, although the pressure inside the dimension damages him due to him not being ready for it. When he recovers eight years later, he begins his search for the culprits to avenge his fallen clan.',
+    author: 'One Leaf In The Sky',
+    publication: '2014',
+    status: 'Completed',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2020/11/almighty-cover-poor.jpg',
+  ),
+
+  //Drama
+  NovelModel(
+    title: 'A Capable Maid',
+    genre: 'Drama',
+    chapters: 45,
+    description: 'Marie, a useless maid who had always been bullied for being not good at anything, experienced something incredible.',
+    author: 'Yuin',
+    publication: '2023',
+    status: 'Completed',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2021/06/A-Capable-Maid.jpg',
+  ),
+  NovelModel(
+    title: 'A Painting of the Villainess as a Young Lady',
+    genre: 'Drama',
+    chapters: 72,
+    description: 'A villainess known by all as a wicked woman who has done all sorts of evil deeds—Ducal Lady Violet.\n\nShe regained her memories of her previous life after she fell into a lake and almost died.\n\nThe very moment she woke up, she immediately visited her father, Duke Everett, and asked one thing.\n\n“Please confine me to the annex.”',
+    author: 'Lititi',
+    publication: '2021',
+    status: 'Ongoing',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2022/04/A-Painting-of-the-Villainess-as-a-Young-Lady.jpg',
+  ),
+  NovelModel(
+    title: 'Becoming Popular Through The “All Villains” Variety Show',
+    genre: 'Drama',
+    chapters: 19,
+    description: 'The variety show “The Most Hated Her and Him” invited six of the most disliked celebrities voted by netizens and industry insiders to participate in the recording.\n\nThe show became a hit as soon as it aired, and netizens called it the most unethical variety show they had ever seen.\n\nHowever, on the day before the official recording of the show, Shang Lu suddenly realized that the world she lived in was a novel and she was a cannon fodder supporting actress, as well as the number one female star of this show, and most hated female celebrity voted by netizens.\n\nShang Lu exclaimed, “VIP viewing area, here I come!”',
+    author: 'Zhuwai Shukuang',
+    publication: '2022',
+    status: 'Ongoing',
+    imageUrl:
+    'https://cdn.novelupdates.com/images/2023/04/Becoming-Popular-Through-The-All-Villains-Variety-Show.jpg',
+  ),
+];
--- novelcomix_app/lib/models/novel_model.dart
+++ novelcomix_app/lib/models/novel_model.dart
@@ -1,17 +1,23 @@
 class NovelModel {
   final String title;
   final String genre;
-  final List<String> chapters;
+  final int chapters;
   final String author;
+  final String description;
+  final String publication;
+  final String status;
   final String imageUrl;
   bool isFavourite;
 
   NovelModel({
-    required this.genre,
-    required this.imageUrl,
     required this.title,
+    required this.genre,
     required this.chapters,
     required this.author,
+    required this.description,
+    required this.publication,
+    required this.status,
+    required this.imageUrl,
     this.isFavourite = false,
   });
 }
